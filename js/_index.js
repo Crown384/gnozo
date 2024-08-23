ae76771a0a3f14482f994547b71abce54909a722
Crown384 <86677683+Crown384@users.noreply.github.com>
2024-08-24 00:56:50 +0100

    hi

---
+++ js/_index.js
@@ -0,0 +1,12 @@
+const engineeringBooks = [
+    {
+        title: "MEE 204 - SENATOR DEX CAMPAIGN",
+        courseCode: "MEE204",
+        lecturer: "Engr James Abioye",
+        ratingStar: "⭐⭐⭐⭐⭐",
+        ratingNum: "4.5",
+        downLink: "https://cotacad.org/mee204.pdf",
+
+        keywords: ['mee204','james','engr','abioye','204','mee','senator','dex']
+    }
+]
