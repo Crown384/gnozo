437e3ed798cba1ffb1c5756bbbf2eed550d2aff7
Crown384 <86677683+Crown384@users.noreply.github.com>
2024-09-10 23:08:13 +0100

    Update LoadBook.js

--- js/LoadBook.js
+++ js/LoadBook.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
             if (container) {
                 // Ensure the container element exists
                 container.innerHTML = "";
-                
+
                 // Create elements to hold the resources
                 const one = document.createElement('div');
                 const two = document.createElement('div');
@@ -18,14 +18,14 @@ document.addEventListener("DOMContentLoaded", function () {
                     let targetDiv;
                     if (index % 3 === 0) {
                         targetDiv = one;
-                    } else if (index % 2 === 0 && (index - 3) % 2 === 0) {
+                    } else if (index % 3 === 1) {
                         targetDiv = two;
                     } else {
                         targetDiv = three;
                     }
 
                     // Append resource content
-                    targetDiv.innerHTML += `<p class="Rbutton">${resource.title}</p>`;
+                    targetDiv.innerHTML += `<p class="btn btn-outline-light Rbutton">${resource.title}</p>`;
                 });
 
                 // Append the divs to the container
