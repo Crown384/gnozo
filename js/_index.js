98c0c77284c2438b1e3e4bba1f4a980718f975fb
Crown384 <86677683+Crown384@users.noreply.github.com>
2024-09-13 00:21:34 +0100

    Thank You JESUS
    AMEN

--- js/_index.js
+++ js/_index.js
@@ -1,12 +1,12 @@
 const engineeringBooks = [
     {
-        title: CHM 101 1st and 2nd order reaction',
+        title: "CHM 101 1st and 2nd order reaction",
         courseCode: "CHM 101",
         author: "",
         ratings: "4.5",
         downLink: "https://drive.google.com/uc?export=download&id=1Qbr7j2cUDR_03XAthhvTa8F4be1YAgnj",
         keywords: ['chm','101','chm 101', 'chm101','order','reaction'],
-semester: "1st semester",
+        semester: "1st semester",
     },
     {
         title: 'CHM101 STARLINK',
