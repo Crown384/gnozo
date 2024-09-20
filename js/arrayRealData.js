b058304b36ea75e6a73bed57f6ca20566a7694fe
Crown384 <86677683+Crown384@users.noreply.github.com>
2024-09-20 21:19:50 +0100

    Update js

---
+++ js/arrayRealData.js
@@ -0,0 +1,63 @@
+const documents = [
+  {
+    title: "CHM 101 1st and 2nd order reaction",
+    fileSize: "20.4K",
+    fileType: "pdf",
+    keywords: [
+      "CHM", "101", "CHM101", "CHM 101", "1st", "2nd", "order", "reaction", 
+      "first", "second", "chemical", "kinetics", "rate", "process", "chemistry"
+    ],
+    courseCode: "CHM 101",
+    semester: "1st semester",
+    downLink: "https://archive.org/download/phy-101-mechanics-and-properties-of-matter-kinematics./CHM%20101%201st%20and%202nd%20order%20reaction..pdf"
+  },
+  {
+    title: "CHM 101 Aromatic Nomenclature Practice Problems",
+    fileSize: "24.0K",
+    fileType: "doc",
+    keywords: [
+      "CHM", "101", "CHM101", "CHM 101", "aromatic", "nomenclature", 
+      "practice", "problems", "organic", "chemistry", "naming", "compounds"
+    ],
+    courseCode: "CHM 101",
+    semester: "1st semester",
+    downLink: "https://archive.org/download/phy-101-mechanics-and-properties-of-matter-kinematics./CHM%20101%20Aromatic%20Nomenclature%20Practice%20Problems..doc"
+  },
+  {
+    title: "CHM 101 Chemical Equilibrium",
+    fileSize: "385.0K",
+    fileType: "pdf",
+    keywords: [
+      "CHM", "101", "CHM101", "CHM 101", "chemical", "equilibrium", "reaction", 
+      "balance", "chemistry", "concentration", "equations", "dynamic"
+    ],
+    courseCode: "CHM 101",
+    semester: "1st semester",
+    downLink: "https://archive.org/download/phy-101-mechanics-and-properties-of-matter-kinematics./CHM%20101%20Chemical%20Equilibrium..pdf"
+  },
+  {
+    title: "GST 101 Communication Listening Skills Reading Skills",
+    fileSize: "unknown",
+    fileType: "pdf",
+    keywords: [
+      "GST", "101", "GST101", "GST 101", "communication", "listening", 
+      "skills", "reading", "understanding", "practice", "comprehension", "study"
+    ],
+    courseCode: "GST 101",
+    semester: "1st semester",
+    downLink: "https://archive.org/download/phy-101-mechanics-and-properties-of-matter-kinematics./GST%20101%20Communication%20Listening%20Skills%20Reading%20Skills.%20%281%29.pdf"
+  },
+  {
+    title: "GST 103 All Together ICT",
+    fileSize: "unknown",
+    fileType: "pdf",
+    keywords: [
+      "GST", "103", "GST103", "GST 103", "all", "together", "ICT", 
+      "information", "communication", "technology", "digital", "learning"
+    ],
+    courseCode: "GST 103",
+    semester: "1st semester",
+    downLink: "https://archive.org/download/phy-101-mechanics-and-properties-of-matter-kinematics./GST%20103%20All%20Together%20ICT.%20%281%29.pdf"
+  }
+];
+
